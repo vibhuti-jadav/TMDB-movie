6f5dfae2611c5c05763a0eccccd9b05a59a0a330
vibhuti-jadav <vibhutij109@gmail.com>
2025-10-09 15:06:09 +0530

    completed

--- src/rtk_Querys/ShowMovieReducer/showMovie.js
+++ src/rtk_Querys/ShowMovieReducer/showMovie.js
@@ -1,18 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const API_KEY = "23f45d0d0053dccafd488246343642e2";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-
 
 export const showMovie = createApi({
   reducerPath: "showmovie",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
   endpoints: (build) => ({
-    //     allMovie:build.query({
-    //   query: ({ endpoint, page = 1,lang="",list="" }) =>
-    //     `/${endpoint}?api_key=${API_KEY}&with_original_language=${lang}&page=${page}&with_genres=${list && list.join(",")}`,
-    // }),
+
     allMovie: build.query({
       query: ({ endpoint, page = 1, lang = "", list = [] }) => {
         const genreParam = list.length ? `&with_genres=${list.join(",")}` : "";
